e81c668847b0496b437a5e47e0c57b903eff11df
Jfrattin <106035011+Jfrattin@users.noreply.github.com>
2024-11-11 11:31:05 -0300

    fixs config.py

--- graficar/graficar.py
+++ graficar/graficar.py
@@ -6,7 +6,6 @@ matplotlib.use('Agg')  # Configura matplotlib para entornos sin GUI
 import matplotlib.pyplot as plt
 from io import BytesIO
 import base64
-import os
 from config import DB_CONNECTIONS
 
 app = Flask(__name__)
